71e7a7b19271226017d2d10ac76ca2d5179d3ee4
David Landup <david.landup.0@gmail.com>
2025-10-12 22:31:52 +0900

    Bump minor version

--- microtrax/__init__.py
+++ microtrax/__init__.py
@@ -5,5 +5,5 @@ microtrax - Local-first, minimalist experiment tracking
 from microtrax.core import init, log, log_images, log_text, finish, serve, ExperimentContext
 from microtrax.enums import ExperimentStatus
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __all__ = ["init", "log", "log_images", "log_text", "finish", "serve", "ExperimentContext", "ExperimentStatus"]
